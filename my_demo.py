d4ed9dd239d5d5502cfd30c4e261ecc9a5cdcefa
Dimitris Papadimitriou <jim_pap@live.com>
2024-04-11 19:55:58 +0300

    changed demos a little bit

--- my_demo.py
+++ my_demo.py
@@ -15,9 +15,9 @@ bw_img = img.convert("L")
 # obtain the underlying np array
 img_array = np.array(bw_img)
 
-eq_trans_of_regions = ahe.calculate_eq_transformations_of_regions(
-    img_array,
-    int(img_array.shape[0]/10),
-    int(img_array.shape[1]/10),
-)
-pass
+ahe_img = ahe.perform_adaptive_hist_equalization_no_interpolation(img_array,int(img_array.shape[0]/5),int(img_array.shape[1]/5))
+plot.figure(1)
+plot.imshow(ahe_img,cmap="gray")
+plot.figure(2)
+plot.imshow(img_array,cmap="gray")
+plot.show()
