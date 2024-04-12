42c06a79d67a287f92d0ed9f6f8afd94d9e14793
Dimitris Papadimitriou <jim_pap@live.com>
2024-04-12 10:22:50 +0300

    added docstring for 'get_equalization_transform_of_img'

---
+++ ahe.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def get_equalization_transform_of_img(
+        img_array:np.ndarray,
+) -> np.ndarray:
+    """
+    Function calculates the histogram of the input image, and then creates
+    the equalization_transform vector as follows:
+        L = 256
+        k = 0,1,...,L-1
+        xk : discreet random variable [0,1,2,...,255]
+        vk : cummulative density function
+        yk : equalization transform
+        yk = round(vk-v0)/(1-v0)*(L-1)
+    
+    Args:
+    img_array (numpy.ndarray): A numpy 2darray, dtype=numpy.uint8 representing
+    an 8-bit grayscale image.
+
+    Returns:
+    equalization_transform (numpy.ndarray): A numpy 1darray, dtype=numpyuint8
+    of size L
+    """
+    pass
