a43bfcefe25fa950cbd8c7f7c2573ca6f7192ded
Dimitris Papadimitriou <jim_pap@live.com>
2024-04-13 19:34:08 +0300

    got systematic results presentation

--- demo.py
+++ demo.py
@@ -15,31 +15,29 @@ bw_img = img.convert("L")
 # obtain the underlying np array
 img_array = np.array(bw_img)
 
-plot.figure(1)
-plot.suptitle("Original Image")
-plot.subplot(121)
-plot.axis("Off")
-plot.imshow(img_array, cmap="gray")
-
+#calculate and present original image along its histogram and cdf
 img_array_hist, img_array_cdf = ahe.get_histogram_of_img(img_array)
-plot.subplot(122)
-plot.bar(np.array(range(ahe.L)),img_array_hist,width=1,label="pdf")
-plot.plot(img_array_cdf*np.max(img_array_hist),'red',label="cdf")
-plot.legend(loc='best')
 
-plot.figure(2)
-plot.suptitle("Global Histogram Equalization")
+fig1,ax1 = plot.subplots(1,2,figsize = (9.5,3.3))
+fig1.suptitle("Original Image")
+ax1[0].axis("off")
+ax1[0].imshow(img_array, cmap="gray")
+ax1[1].bar(np.array(range(ahe.L)), img_array_hist, width=1, label="pdf")
+ax1[1].plot(img_array_cdf*np.max(img_array_hist), 'red', label="cdf")
+ax1[1].legend()
+#fig1.savefig("save_test.png")
 
-equalized_img = ahe.perform_global_hist_equalization(img_array)
-plot.subplot(121)
-plot.axis("off")
-plot.imshow(equalized_img,cmap="gray")
+#calculate and present image after global histogram equalization along with
+#it's histogram and cdf
+equalized_img_array = ahe.perform_global_hist_equalization(img_array)
+equalized_img_hist,equalized_img_cdf = ahe.get_histogram_of_img(equalized_img_array)
 
-equalized_img_hist,equalized_img_cdf = ahe.get_histogram_of_img(equalized_img)
-plot.subplot(122)
-plot.bar(np.array(range(ahe.L)), equalized_img_hist, width=1, label="pdf")
-plot.plot(equalized_img_cdf*np.max(equalized_img_hist),'red',label="cdf")
-plot.legend(loc='best')
-
+fig2,ax2 = plot.subplots(1,2,figsize = (9.5,3.3))
+fig2.suptitle("Global Histogram Equalization")
+ax2[0].axis("off")
+ax2[0].imshow(equalized_img_array, cmap="gray")
+ax2[1].bar(np.array(range(ahe.L)), equalized_img_hist, width=1, label="pdf")
+ax2[1].plot(img_array_cdf*np.max(equalized_img_hist), 'red', label="cdf")
+ax2[1].legend()
 
 plot.show()
