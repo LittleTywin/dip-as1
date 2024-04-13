6dcbc960c65513410796ee34ccac8a5d8bdd2f41
Dimitris Papadimitriou <jim_pap@live.com>
2024-04-13 19:08:06 +0300

    got somewhat acceptable result representation

--- demo.py
+++ demo.py
@@ -19,12 +19,27 @@ plot.figure(1)
 plot.suptitle("Original Image")
 plot.subplot(121)
 plot.axis("Off")
-plot.title("Image")
 plot.imshow(img_array, cmap="gray")
 
-img_array_hist = ahe.get_histogram_of_img(img_array)
+img_array_hist, img_array_cdf = ahe.get_histogram_of_img(img_array)
 plot.subplot(122)
-plot.title("Histogram")
-plot.bar(np.array(range(256)),img_array_hist,width=1)
+plot.bar(np.array(range(ahe.L)),img_array_hist,width=1,label="pdf")
+plot.plot(img_array_cdf*np.max(img_array_hist),'red',label="cdf")
+plot.legend(loc='best')
+
+plot.figure(2)
+plot.suptitle("Global Histogram Equalization")
+
+equalized_img = ahe.perform_global_hist_equalization(img_array)
+plot.subplot(121)
+plot.axis("off")
+plot.imshow(equalized_img,cmap="gray")
+
+equalized_img_hist,equalized_img_cdf = ahe.get_histogram_of_img(equalized_img)
+plot.subplot(122)
+plot.bar(np.array(range(ahe.L)), equalized_img_hist, width=1, label="pdf")
+plot.plot(equalized_img_cdf*np.max(equalized_img_hist),'red',label="cdf")
+plot.legend(loc='best')
+
 
 plot.show()
