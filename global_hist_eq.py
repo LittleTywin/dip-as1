0f1ad0f2ec90017cbe8fffe01c9ea5309a4ed0d8
Dimitris Papadimitriou <jim_pap@live.com>
2024-04-14 15:40:25 +0300

    implemented global_hist_equalization.py

--- global_hist_eq.py
+++ global_hist_eq.py
@@ -1,12 +1,13 @@
 import numpy as np
 
+L=256
+
 def get_histogram_cdf_of_img(
         img_array: np.ndarray,
 )-> tuple:
     """
     Calculates histogram and cumulative density function of input image.
     """
-    L=256
     hist = np.zeros(L,dtype=np.float32)
     cdf = np.zeros(L,dtype=np.float32)
     img_size = img_array.size
@@ -28,9 +29,19 @@ def get_equalization_transform_of_img(
     equalization_transform(numpy.ndarray): A numpy array with ndim=1,
     dtype=numpy.uint8
     """
-    pass
+    equalization_transform = np.zeros(L, dtype=np.uint8)
+    _,cdf = get_histogram_cdf_of_img(img_array)
+    for i in L:
+        equalization_transform[i] = round((cdf[i]-cdf[0])/(1-cdf[0])*(L-1))
+
+    return equalization_transform
 
 def perform_global_hist_equalization(
         img_array: np.ndarray,
 ) -> np.ndarray:
-    pass
+    ret_img_array = np.zeros(img_array.shape,dtype=np.uint8)
+    equalization_transform = get_equalization_transform_of_img(img_array)
+    for i in range(L):
+        ret_img_array[img_array==L] = equalization_transform[i]
+    
+    return ret_img_array
